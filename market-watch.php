bc664ed21d6ed933383ca1d667104210fc55a2a9
Max Base <MaxBaseCode@Gmail.Com>
2021-01-03 20:52:45 +0330

    Database connection mysql php

--- market-watch.php
+++ market-watch.php
@@ -6,3 +6,36 @@
  * @Repository: https://github.com/BaseMax/tehran-stock-market-watch-php/
  */
 
+function arabicToPersian($str) {
+  return $str;
+}
+
+function n2n($num) {
+  return round($num, 2);
+}
+
+function countDigits($input) {
+  return 0;
+}
+
+function getDbConnection() {
+    global $pdo;
+    $host = 'localhost';
+    $db   = 'stock';
+    $user = 'root';
+    $pass = '';
+    $port = '3306';
+    $charset = 'utf8mb4';
+
+    $options = [
+        \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        \PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+    try {
+        $pdo = new \PDO($dsn, $user, $pass, $options);
+    } catch (\PDOException $e) {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+}
